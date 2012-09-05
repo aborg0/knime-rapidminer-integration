0487ff5f7b7afdebf292baf5bfbb52329042c67b
aborg <aborg@localhost>
2012-09-05 11:08:37 +0000

    Added licensing notes.

--- com.mind_era.knime_rapidminer.knime.nodes.local/src/com/mind_era/knime_rapidminer/knime/nodes/RapidMinerInit.java
+++ com.mind_era.knime_rapidminer.knime.nodes.local/src/com/mind_era/knime_rapidminer/knime/nodes/RapidMinerInit.java
@@ -1,5 +1,18 @@
-/**
- * 
+/*  RapidMiner Integration for KNIME
+ *  Copyright (C) 2012 Mind Eratosthenes Kft.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.mind_era.knime_rapidminer.knime.nodes;
 
@@ -7,7 +20,6 @@ import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.knime.core.node.defaultnodesettings.HasTableSpecAndRowId;
 
 import com.mind_era.knime_rapidminer.knime.nodes.internal.RapidMinerNodePlugin;
